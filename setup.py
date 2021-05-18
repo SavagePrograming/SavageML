2933878f4e680f0c07670a3c64f723e83e25c13c
savagewil <wxs5474@rit.edu>
2021-05-17 20:19:56 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SavageML',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT License',
+    author='William Savage',
+    author_email='savage.programing@gmail.com',
+    description='A Personal Experimental Machine Learning Library'
+)
