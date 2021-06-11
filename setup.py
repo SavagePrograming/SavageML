19cc2e313537d19489eccefad94307ff3ccbfbe5
savagewil <wxs5474@rit.edu>
2021-06-11 17:04:13 -0400

    Adding verify function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SavageML',
-    version='0.1.2',
+    version='0.1.4',
     packages=['savageml'],
     url='https://github.com/SavagePrograming/SavageML',
     license='MIT License',
