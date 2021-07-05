f7fc2ffb4ac748128f3f4d8ec2577083c9ac81ef
savagewil <wxs5474@rit.edu>
2021-07-05 14:39:25 -0400

    Updated Dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements("requirements.txt")
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='SavageML',
-    version='0.1.0.3',
+    version='0.1.1',
     packages=['savageml', 'savageml.models', 'savageml.simulations'],
     url='https://github.com/SavagePrograming/SavageML',
     license='MIT License',
@@ -15,8 +11,7 @@ setup(
     description='A Personal Experimental Machine Learning Library',
     python_requires='>=3',
     install_requires=[
-        "pytest>=6.2.4"
-        "scikit-learn>=0.24.2",
-        "numpy>=1.21.0"
+        "scikit-learn",
+        "numpy"
     ]
 )
