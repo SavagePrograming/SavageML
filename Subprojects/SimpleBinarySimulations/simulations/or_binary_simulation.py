791ee104baae409c9f23791f0a75ad83a4d3337f
wsavage <willsvg@gmail.com>
2021-07-16 22:21:47 -0400

    Setting up other binary simulations

--- Subprojects/SimpleBinarySimulations/simulations/or_binary_simulation.py
+++ Subprojects/SimpleBinarySimulations/simulations/or_binary_simulation.py
@@ -1,15 +1,27 @@
-from savageml.simulations import BaseSimulation
+from ..utility import LossFunctions
+import numpy as np
+from savageml.simulations import BaseSimulation, SimulationState
 
 
 class OrBinarySimulation(BaseSimulation):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, shape=(1,), max_steps=10, loss_function=LossFunctions.MSE, **kwargs):
+        super().__init__(**kwargs)
+        self.shape = shape
+        self.max_steps = max_steps
+        self.step_count = 0
+        self.loss_function = loss_function
 
     def step(self, visualize=False) -> tuple:
-        pass
-
-    def __iter__(self):
-        pass
-
-    def reset(self):
-        pass
+        if self.step_count >= self.max_steps:
+            self.state = SimulationState.COMPLETE
+            return ()
+        else:
+            self.step_count += 1
+            if self.step_count >= self.max_steps:
+                self.state = SimulationState.COMPLETE
+            else:
+                self.state = SimulationState.RUNNING
+            sample = self.random.choice([0.0, 1.], self.shape)
+            output = np.array(((sample == 1.0).any())).astype(float)
+            prediction = self.model.predict(sample)
+            return sample, output, self.loss_function(output, prediction)
