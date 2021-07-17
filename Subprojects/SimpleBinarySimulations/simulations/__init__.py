b1d71c9c4c57f54ed1a8005cb9c838c6932f3657
wsavage <willsvg@gmail.com>
2021-07-16 22:25:33 -0400

    Setting up tests for binary simulations

--- Subprojects/SimpleBinarySimulations/simulations/__init__.py
+++ Subprojects/SimpleBinarySimulations/simulations/__init__.py
@@ -1,1 +1,4 @@
 from .reflexive_binary_simulation import ReflexiveBinarySimulation
+from .and_binary_simulation import AndBinarySimulation
+from .or_binary_simulation import OrBinarySimulation
+from .xor_binary_simulation import XorBinarySimulation
