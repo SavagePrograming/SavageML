da3e14375e44e49c9387e86c63c7df9bbe74bf2e
wsavage <willsvg@gmail.com>
2021-07-16 22:58:24 -0400

    Adding binary simulations to library

--- savageml/simulations/__init__.py
+++ savageml/simulations/__init__.py
@@ -1,2 +1,6 @@
 from .base_simulation import BaseSimulation
 from .simulation_state import SimulationState
+from .reflexive_binary_simulation import ReflexiveBinarySimulation
+from .and_binary_simulation import AndBinarySimulation
+from .or_binary_simulation import OrBinarySimulation
+from .xor_binary_simulation import XorBinarySimulation
