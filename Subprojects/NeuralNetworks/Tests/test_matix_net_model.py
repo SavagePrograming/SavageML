5457b7c957ac799b524589dc1a92904020fc3ac8
wsavage <willsvg@gmail.com>
2021-07-22 10:58:41 -0400

    IT Learns!

--- Subprojects/NeuralNetworks/Tests/test_matix_net_model.py
+++ Subprojects/NeuralNetworks/Tests/test_matix_net_model.py
@@ -8,7 +8,7 @@ DATA_SIZE = 100
 DATA_WIDTH = 2
 OUT_WIDTH = 1
 DIMENSIONS = [DATA_WIDTH, 2, OUT_WIDTH]
-SIMULATION = XorBinarySimulation((1, DATA_WIDTH), DATA_SIZE)
+SIMULATION = XorBinarySimulation((DATA_WIDTH,), DATA_SIZE)
 RNG = numpy.random.default_rng(0)
 TEST_DATA = RNG.choice([0.0, 1.0], (DATA_SIZE, DATA_WIDTH))
 TEST_XOR = np.reshape((TEST_DATA[:, 0] != TEST_DATA[:, 1]), (DATA_SIZE, 1))
@@ -37,3 +37,25 @@ def test_fit_simulation():
     model = MatrixNetModel(DIMENSIONS)
     model.fit(SIMULATION, learning_rate=LEARNING_RATE)
     assert True
+
+
+def test_fit_simulation_score():
+    model = MatrixNetModel(DIMENSIONS)
+    simulation = XorBinarySimulation((DATA_WIDTH,), 50000, seed=0)
+    # print()
+    # print(model.predict(np.array([[1.0, 0.0]])))
+    # print(model.predict(np.array([[0.0, 1.0]])))
+    # print(model.predict(np.array([[1.0, 1.0]])))
+    # print(model.predict(np.array([[0.0, 0.0]])))
+
+    model.fit(simulation, learning_rate=LEARNING_RATE, batch_size=10)
+
+    print(model.predict(np.array([[1.0, 0.0]])), model.predict(np.array([[1.0, 0.0]])) > .5)
+    print(model.predict(np.array([[0.0, 1.0]])), model.predict(np.array([[0.0, 1.0]])) > .5)
+    print(model.predict(np.array([[1.0, 1.0]])), model.predict(np.array([[1.0, 1.0]])) > .5)
+    print(model.predict(np.array([[0.0, 0.0]])), model.predict(np.array([[0.0, 0.0]])) > .5)
+
+    assert model.predict(np.array([[1.0, 0.0]])) > .75
+    assert model.predict(np.array([[0.0, 1.0]])) > .75
+    assert model.predict(np.array([[0.0, 0.0]])) < .25
+    assert model.predict(np.array([[1.0, 1.0]])) < .25
--- Subprojects/NeuralNetworks/utility/generic_functions.py
+++ Subprojects/NeuralNetworks/utility/generic_functions.py
@@ -1,6 +1,25 @@
+import numpy as np
+
+
 def get_sample_from_iterator(data_iterator):
     try:
         sample = data_iterator.__next__()
         return True, sample
     except StopIteration:
         return False, None
+
+
+def batch_iterator(iterable, batch_size=1):
+    batch_array = []
+    for data in iterable:
+        batch_array.append(data)
+        if len(batch_array) >= batch_size:
+            yield batch_array
+            batch_array = []
+    if len(batch_array) > 0:
+        return batch_array
+
+
+def batch_np_array(array: np.ndarray, batch_size=1):
+    for batch_index in range(0, array.shape[0], batch_size):
+        yield array[batch_index:min(batch_index + batch_size, array.shape[0])]
