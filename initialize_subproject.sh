533d8b868d825f871972e88dd4faa46ebeb95eea
savagewil <willsvg@gmail.com>
2021-08-11 11:08:45 -0400

    Adding binary simulations

--- initialize_subproject.sh
+++ initialize_subproject.sh
@@ -1,4 +1,5 @@
 mkdir "./Subprojects/$1"
+touch "./Subprojects/$1/__init__.py"
 mkdir "./Subprojects/$1/models"
 touch "./Subprojects/$1/models/__init__.py"
 git add "./Subprojects/$1/models/__init__.py"
