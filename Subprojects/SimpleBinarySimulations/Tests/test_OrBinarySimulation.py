41c55b33233af803a5ef73cae6584f4687f65363
savagewil <willsvg@gmail.com>
2021-08-11 11:08:45 -0400

    Setting up tests for binary simulations

--- Subprojects/SimpleBinarySimulations/Tests/test_OrBinarySimulation.py
+++ Subprojects/SimpleBinarySimulations/Tests/test_OrBinarySimulation.py
@@ -1,33 +1,33 @@
 from savageml.simulations import SimulationState
-from ..simulations import ReflexiveBinarySimulation
+from ..simulations import OrBinarySimulation
 
 
 def test_initial_state_initialized():
-    simulation = ReflexiveBinarySimulation()
+    simulation = OrBinarySimulation()
     assert simulation.get_state() == SimulationState.INITIALIZED
 
 
 def test_state_after_run_completed():
-    simulation = ReflexiveBinarySimulation()
+    simulation = OrBinarySimulation()
     simulation.run()
     assert simulation.get_state() == SimulationState.COMPLETE
 
 
 def test_state_after_step_not_initialized():
-    simulation = ReflexiveBinarySimulation()
+    simulation = OrBinarySimulation()
     simulation.step()
     assert simulation.get_state() != SimulationState.INITIALIZED
 
 
 def test_reset_state_initialized():
-    simulation = ReflexiveBinarySimulation()
+    simulation = OrBinarySimulation()
     simulation.step()
     simulation.reset()
     assert simulation.get_state() == SimulationState.INITIALIZED
 
 
 def test_reflexive_simulation_duplicates():
-    simulation = ReflexiveBinarySimulation(seed=1)
+    simulation = OrBinarySimulation(seed=1)
     simulation_duplicate = simulation.__iter__()
     assert simulation.get_seed() == simulation_duplicate.get_seed()
     assert simulation.step() == simulation_duplicate.step()
@@ -35,7 +35,7 @@ def test_reflexive_simulation_duplicates():
 
 def test_reflexive_simulation_shape_correct():
     test_shape = (5, 7)
-    simulation = ReflexiveBinarySimulation(seed=1, shape=test_shape)
+    simulation = OrBinarySimulation(seed=1, shape=test_shape)
     result = simulation.step()
     assert result[0].shape == test_shape
     assert result[1].shape == test_shape
@@ -43,13 +43,13 @@ def test_reflexive_simulation_shape_correct():
 
 def test_reflexive_simulation_reflexive():
     test_shape = (5, 7)
-    simulation = ReflexiveBinarySimulation(seed=1, shape=test_shape)
+    simulation = OrBinarySimulation(seed=1, shape=test_shape)
     result = simulation.step()
     assert (result[0] == result[1]).all()
 
 def test_reflexive_simulation_step_count():
     max_steps = 11
-    simulation = ReflexiveBinarySimulation(seed=1, max_steps=max_steps)
+    simulation = OrBinarySimulation(seed=1, max_steps=max_steps)
     count = 0
     for _ in simulation:
         count += 1
